d8830e291b46178a8141db1b33fd7408a927f472
Lukas Betancur <betancurlukas98@gmail.com>
2020-03-23 10:53:02 -0400

    commit 10

--- src/Exercises.java
+++ src/Exercises.java
@@ -233,7 +233,54 @@ public class Exercises {
   }
 
   public ArrayList<Integer> merge(ArrayList<Integer> list, boolean ascending) {
-    return null;
+    if (list == null || list.size() < 1) {
+      return null;
+    }
+
+    if (list.size() == 1) {
+        return list;
+    }
+
+
+    ArrayList <Integer> right = new ArrayList <Integer> ();
+    ArrayList <Integer> left = new ArrayList <Integer> ();
+    for (int i = 0; i < list.size() / 2; i++) {
+        left.add(list.get(i));
+    }
+
+    for (int i = list.size() / 2; i < list.size(); i++) {
+        right.add(list.get(i));
+    }
+    right = merge(right, ascending);
+    left = merge(left, ascending);
+
+    ArrayList <Integer> sortedList = new ArrayList <Integer> ();
+    while (right.size() != 0 && left.size() != 0) {
+        if (ascending) {
+            if (right.get(0) < left.get(0)) {
+                sortedList.add(right.get(0));
+                right.remove(0);
+            } else {
+                sortedList.add(left.get(0));
+                left.remove(0);
+            }
+        } else {
+            if (right.get(0) > left.get(0)) {
+                sortedList.add(right.get(0));
+                right.remove(0);
+            } else {
+                sortedList.add(left.get(0));
+                left.remove(0);
+            }
+        }
+    }
+    if (right.size() > 0) {
+        sortedList.addAll(right);
+    } else if (left.size() > 0) {
+        sortedList.addAll(left);
+    }
+
+    return sortedList;
   }
 
   public String[] merge(String[] list, boolean ascending) {
