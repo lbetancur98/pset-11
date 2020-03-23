080d31731b433a17cf53ebf756cfccb9ea30ffd1
Lukas Betancur <betancurlukas98@gmail.com>
2020-03-23 10:41:30 -0400

    commit 2

--- src/Exercises.java
+++ src/Exercises.java
@@ -31,8 +31,29 @@ public class Exercises {
   }
 
   public int findMeFaster(ArrayList<Integer> list, int target) {
+    if (list == null) {
+      return -1;
+    }
+
+    int lower = 0;
+    int mid = 0;
+    int upper = list.size();
+
+
+    while (lower <= upper) {
+      mid = (upper + lower) / 2;
+
+      if (target > list.get(mid)) {
+          lower = mid + 1;
+      } else if (target < list.get(mid)) {
+          upper = mid - 1;
+      } else {
+          return mid;
+      }
+   }
+    
     return -1;
-  }
+    }
 
   public int findMeFaster(String[] list, String target) {
     return -1;
